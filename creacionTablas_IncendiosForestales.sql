26d72bb19d09490e58f37ec6e2360c28b6a532fe
hyunsap <116818310+hyunsap@users.noreply.github.com>
2025-05-16 16:53:38 -0300

    Add files via upload

---
+++ creacionTablas_IncendiosForestales.sql
@@ -0,0 +1,197 @@
+-- Tabla Bosques
+CREATE TABLE Bosques (
+    nombre TEXT PRIMARY KEY,
+    superficie NUMERIC
+);
+
+-- Tabla AfluenciasTuristicas
+CREATE TABLE AfluenciasTuristicas (
+    fecha DATE,
+    nombre_bosque TEXT,
+    cant_turistas INTEGER,
+    PRIMARY KEY (fecha, nombre_bosque),
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre)
+);
+
+-- Tabla Polígonos
+CREATE TABLE Poligonos (
+    id_poligono SERIAL PRIMARY KEY,
+    nombre_bosque TEXT,
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre)
+);
+
+-- Tabla Coordenadas
+CREATE TABLE Coordenadas (
+    latitud NUMERIC,
+    longitud NUMERIC,
+    id_poligono INTEGER,
+    n_coordenadas INTEGER,
+    PRIMARY KEY (latitud, longitud),
+    FOREIGN KEY (id_poligono) REFERENCES Poligonos(id_poligono)
+);
+
+-- Tabla IncendiosForestales
+CREATE TABLE IncendiosForestales (
+    ts_inicio TIMESTAMP,
+    nombre_bosque TEXT,
+    ts_fin TIMESTAMP,
+    estacion TEXT,
+    hect_quemadas NUMERIC,
+    latitud_inicio NUMERIC,
+    longitud_inicio NUMERIC,
+    PRIMARY KEY (ts_inicio, nombre_bosque),
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre)
+);
+
+
+
+-- Tabla IndicesGLI
+CREATE TABLE IndicesGLI (
+    fecha DATE,
+    nombre_bosque TEXT,
+    valor_gli NUMERIC,
+    valor_rojo NUMERIC,
+    valor_azul NUMERIC,
+    valor_verde NUMERIC,
+    PRIMARY KEY (fecha, nombre_bosque),
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre)
+);
+
+-- Tabla IndicesSAVI
+CREATE TABLE IndicesSAVI (
+    fecha DATE,
+    nombre_bosque TEXT,
+    valor_savi NUMERIC,
+    PRIMARY KEY (fecha, nombre_bosque),
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre)
+);
+
+-- Tabla Partidos
+CREATE TABLE Partidos (
+    nombre TEXT PRIMARY KEY,
+    densidad_poblacional NUMERIC,
+    superficie_total NUMERIC
+);
+
+-- Tabla BosquesEnPartidos
+CREATE TABLE BosquesEnPartidos (
+    nombre_bosque TEXT,
+    nombre_partido TEXT,
+    PRIMARY KEY (nombre_bosque, nombre_partido),
+    FOREIGN KEY (nombre_bosque) REFERENCES Bosques(nombre),
+    FOREIGN KEY (nombre_partido) REFERENCES Partidos(nombre)
+);
+
+-- Tabla EstacionesMetereológicas
+CREATE TABLE EstacionesMetereologicas (
+    nombre TEXT PRIMARY KEY,
+    nombre_partido TEXT,
+    calle TEXT,
+    numero TEXT,
+    mail TEXT,
+    telefono TEXT,
+    FOREIGN KEY (nombre_partido) REFERENCES Partidos(nombre)
+);
+
+-- Tabla InformesMetereológicos
+CREATE TABLE InformesMetereologicos (
+    timestamp TIMESTAMP,
+    nombre_estacionMetereologica TEXT,
+    dir_viento TEXT,
+    vel_viento NUMERIC,
+    precipitacion_6h NUMERIC,
+    precipitacion12_h NUMERIC,
+    humedad NUMERIC,
+    temperatura NUMERIC,
+    PRIMARY KEY (timestamp, nombre_estacionMetereologica),
+    FOREIGN KEY (nombre_estacionMetereologica) REFERENCES EstacionesMetereologicas(nombre)
+);
+
+-- Tabla ComportamientosIncendios
+CREATE TABLE ComportamientosIncendios (
+    nombre_bosque TEXT,
+    inicio_incendio TIMESTAMP,
+    dia DATE,
+    hora TIME,
+    longitud_llamas NUMERIC,
+    altura_llamas NUMERIC,
+    humedad_comb_FFMC NUMERIC,
+    humedad_comb_DMC NUMERIC,
+    humedad_comb_DC NUMERIC,
+    timestamp_informeMeteorologico TIMESTAMP,
+    nombre_estacionMetereologica TEXT,
+    PRIMARY KEY (nombre_bosque, inicio_incendio, dia, hora),
+    FOREIGN KEY (nombre_bosque, inicio_incendio) REFERENCES IncendiosForestales(nombre_bosque, ts_inicio),
+    FOREIGN KEY (timestamp_informeMeteorologico, nombre_estacionMetereologica)
+        REFERENCES InformesMetereologicos(timestamp, nombre_estacionMetereologica)
+);
+
+-- Tabla Bomberos
+CREATE TABLE Bomberos (
+    nro_brigada INTEGER PRIMARY KEY,
+    nombre_partido TEXT,
+    tipo TEXT,
+    cantidad_bomberos INTEGER,
+    FOREIGN KEY (nombre_partido) REFERENCES Partidos(nombre)
+);
+
+-- Tabla BomberosEnIncendios
+CREATE TABLE BomberosEnIncendios (
+    nro_brigada INTEGER,
+    nombre_bosque TEXT,
+    inicio_incendio TIMESTAMP,
+    PRIMARY KEY (nro_brigada, nombre_bosque, inicio_incendio),
+    FOREIGN KEY (nro_brigada) REFERENCES Bomberos(nro_brigada),
+    FOREIGN KEY (nombre_bosque, inicio_incendio) REFERENCES IncendiosForestales(nombre_bosque, ts_inicio)
+);
+
+-- Tabla Recursos
+CREATE TABLE Recursos (
+    nombre TEXT PRIMARY KEY,
+    tipo TEXT,
+    descripcion TEXT
+);
+
+-- Tabla RecursosUtilizadosEnIncendios
+CREATE TABLE RecursosUtilizadosEnIncendios (
+    nombre_recurso TEXT,
+    nombre_bosque TEXT,
+    inicio_incendio TIMESTAMP,
+    cantidad INTEGER,
+    PRIMARY KEY (nombre_recurso, nombre_bosque, inicio_incendio),
+    FOREIGN KEY (nombre_recurso) REFERENCES Recursos(nombre),
+    FOREIGN KEY (nombre_bosque, inicio_incendio) REFERENCES IncendiosForestales(nombre_bosque, ts_inicio)
+);
+
+-- Tabla Tácticas
+CREATE TABLE Tacticas (
+    nombre TEXT PRIMARY KEY,
+    descripcion TEXT
+);
+
+-- Tabla TácticasUtilizadasEnIncendios
+CREATE TABLE TacticasUtilizadasEnIncendios (
+    nombre_tactica TEXT,
+    nombre_bosque TEXT,
+    inicio_incendio TIMESTAMP,
+    PRIMARY KEY (nombre_tactica, nombre_bosque, inicio_incendio),
+    FOREIGN KEY (nombre_tactica) REFERENCES Tacticas(nombre),
+    FOREIGN KEY (nombre_bosque, inicio_incendio) REFERENCES IncendiosForestales(nombre_bosque, ts_inicio)
+);
+
+-- Tabla Causas
+CREATE TABLE Causas (
+    nombre TEXT PRIMARY KEY,
+    tipo TEXT,
+    descripcion TEXT
+);
+
+-- Tabla CausasIncendios
+CREATE TABLE CausasIncendios (
+    nombre_causa TEXT,
+    nombre_bosque TEXT,
+    inicio_incendio TIMESTAMP,
+    PRIMARY KEY (nombre_causa, nombre_bosque, inicio_incendio),
+    FOREIGN KEY (nombre_causa) REFERENCES Causas(nombre),
+    FOREIGN KEY (nombre_bosque, inicio_incendio) REFERENCES IncendiosForestales(nombre_bosque, ts_inicio)
+);
